5c380518582bd7af1fce41314023b5506bc115c0
gorkemyagci <gorkemyagci705@gmail.com>
2024-08-14 16:57:05 +0300

    smartdoc

--- components/pdf-renderer/index.tsx
+++ components/pdf-renderer/index.tsx
@@ -1,11 +1,43 @@
-const PdfRenderer = () => {
-    return <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
-        <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-            <div className="flex items-center gap-1.5">
-                top bar
-            </div>
+"use client";
+import { Loader2 } from "lucide-react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+import { useToast } from "../ui/use-toast";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
+
+const PdfRenderer = ({ url }: { url: string }) => {
+  const { toast } = useToast();
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">top bar</div>
+      </div>
+      <div className="flex-1 w-full max-h-screen">
+        <div>
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={(error) => {
+              toast({
+                title: "Error loading PDF",
+                description: "Please try again later.",
+                variant: "destructive",
+              });
+            }}
+            file={`https://utfs.io/f/${url}`}
+            className="max-h-full"
+          >
+            <Page pageNumber={1} />
+          </Document>
         </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default PdfRenderer;
